4f06b7c64edced808dc7d5941bec94f4d273ecd5
selveresta <artem.lypnyk@softsculptor.net>
2023-04-16 15:27:48 +0300

    tesdt

--- server/test/brandController.test.js
+++ server/test/brandController.test.js
@@ -2,8 +2,12 @@ const brandController = require("../controllers/brandController");
 
 test("getAllBrands", async () => {
 	let brands;
-	await fetch("http://localhost:5000/api/brand").then((data) => {
-		brands = data;
-	});
-	expect(brands).toBeDefined();
+	try {
+		await fetch("http://localhost:5000/api/brand").then((data) => {
+			brands = data;
+		});
+		expect(brands).toBeDefined();
+	} catch (error) {
+		expect(brands).toBeUndefined();
+	}
 });
--- server/test/typeController.test.js
+++ server/test/typeController.test.js
@@ -2,8 +2,12 @@ const typeController = require("../controllers/typeController");
 
 test("getAllTypes", async () => {
 	let types;
-	await fetch("http://localhost:5000/api/type").then((data) => {
-		types = data;
-	});
-	expect(types).toBeDefined();
+	try {
+		await fetch("http://localhost:5000/api/type").then((data) => {
+			types = data;
+		});
+		expect(types).toBeDefined();
+	} catch (error) {
+		expect(types).toBeUndefined();
+	}
 });
